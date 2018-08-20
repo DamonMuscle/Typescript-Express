45ad2f75412757ba75bd0b83e2fb89095c471ae1
damon.huang <damon.huang@transfinder.com>
2018-08-20 13:50:32 +0800

    add user control logic

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ const mongoUrl = MONGODB_URI;
 mongoose.connect(mongoUrl, { useMongoClient: true }).then(
 	() => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
 ).catch(err => {
-	console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+	console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
 	// process.exit();
 });
 
@@ -52,8 +52,8 @@ app.use(session({
 		autoReconnect: true
 	})
 }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(flash());
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
@@ -61,20 +61,20 @@ app.use((req, res, next) => {
 	res.locals.user = req.user;
 	next();
 });
-app.use((req, res, next) => {
-	// After successful login, redirect back to the intended page
-	if (!req.user &&
-		req.path !== "/login" &&
-		req.path !== "/signup" &&
-		!req.path.match(/^\/auth/) &&
-		!req.path.match(/\./)) {
-		req.session.returnTo = req.path;
-	} else if (req.user &&
-		req.path == "/account") {
-		req.session.returnTo = req.path;
-	}
-	next();
-});
+// app.use((req, res, next) => {
+// 	// After successful login, redirect back to the intended page
+// 	if (!req.user &&
+// 		req.path !== "/login" &&
+// 		req.path !== "/signup" &&
+// 		!req.path.match(/^\/auth/) &&
+// 		!req.path.match(/\./)) {
+// 		req.session.returnTo = req.path;
+// 	} else if (req.user &&
+// 		req.path == "/account") {
+// 		req.session.returnTo = req.path;
+// 	}
+// 	next();
+// });
 
 app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }));
 
@@ -83,5 +83,7 @@ app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }))
  */
 app.get("/", homeController.index);
 app.post("/api/user/register", userController.register);
+app.post("/api/user/login", userController.login);
+app.post("/api/user/update", userController.update);
 
 export default app;
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import user from "../models/user";
+import { md5 } from "../util/md5";
 
 /**
  * register user
@@ -9,11 +10,34 @@ import user from "../models/user";
 export let register = (req: Request, res: Response) => {
 	new user({
 		email: req.body.email,
-		password: req.body.password,
+		password: md5(req.body.password),
 		userName: req.body.userName
 	}).save().then((r) => {
 		res.sendStatus(200);
 	}).catch((e) => {
 		res.json(e.errmsg);
 	});
+};
+
+export let login = (req: Request, res: Response) => {
+	user.find({
+		email: req.body.email,
+		password: md5(req.body.password)
+	}, (err, users) => {
+		if (err) res.send(err);
+
+		if (users && users.length == 1) {
+			res.send(true);
+			req.session.currentUid = users[0].id;
+			req.session.save(() => { });
+		}
+		else res.send("Email or password is incorrect.");
+	});
+};
+
+export let update = (req: Request, res: Response) => {
+	user.findByIdAndUpdate(req.session.currentUid,
+		{ password: md5(req.body.password) }, (err, result) => {
+			res.send(result)
+		});
 };
